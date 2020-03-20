5c47056d7a2eb87c5c21319d5960388fc1903b4b
Nihal S <nihalsreenivasu@gmail.com>
2020-03-20 15:06:59 +0530

    may be

--- ride/rides.py
+++ ride/rides.py
@@ -377,4 +377,4 @@ def ride_count():
 
 if __name__ == '__main__':
 	app.debug=True    
-	app.run(host="0.0.0.0",port=80)
+	app.run(host="0.0.0.0",port=8000)
--- user/users.py
+++ user/users.py
@@ -205,4 +205,4 @@ def count_reset():
 
 if __name__ == '__main__':
 	app.debug=True    
-	app.run( host="0.0.0.0",port=80)
+	app.run( host="0.0.0.0",port=8080)
